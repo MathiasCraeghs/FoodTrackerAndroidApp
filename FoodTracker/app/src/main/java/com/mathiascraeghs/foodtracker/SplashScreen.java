1cb97931560097bd82c0540cda83b7d9f0be1366
DESKTOP-343A0BU\Mathias <mathias.craeghs@student.uhasselt.be>
2018-04-12 15:34:23 +0200

    no message

--- FoodTracker/app/src/main/java/com/mathiascraeghs/foodtracker/SplashScreen.java
+++ FoodTracker/app/src/main/java/com/mathiascraeghs/foodtracker/SplashScreen.java
@@ -1,5 +1,7 @@
 package com.mathiascraeghs.foodtracker;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        int secondsDelayed = 5;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, secondsDelayed * 1000);
     }
 }
